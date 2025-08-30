05d78c63678ff34a9f178520ed82f240c56aaf76
Holence <Holence08@gmail.com>
2025-08-30 13:26:42 +0800

    patch Cool autostart

--- config.def.h
+++ config.def.h
@@ -18,6 +18,13 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_main,  col_main  },
 };
 
+/* cool-autostart patch */
+static const char *const autostart[] = {
+	"diodon", NULL, // clipboard daemon
+	// "swanctlgui_run", NULL,
+	NULL /* terminate */
+};
+
 /* systray patch */
 static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0; /* 0: systray in the right corner, >0: systray on left of status text */
